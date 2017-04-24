1f517deb8362a64abcae53a327a10f673308e87e
Alexander Schleifer <alexander.schleifer@ise.de>
2017-04-24 14:57:08 +0200

    Added some deps.

--- happyshop-rpibootstrap.sh
+++ happyshop-rpibootstrap.sh
@@ -15,9 +15,15 @@ fi
 
 sudo /bin/sh -e - << EOF
 
+  MYSQL_SERVER_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
+
   apt update
   apt upgrade -y
-  apt install -y mono-runtime unzip git curl xinit xserver-xorg ttf-anonymous-pro unclutter fonts-fantasque-sans
+
+  debconf-set-selections <<< "mysql-server mysql-server/root_password password ${MYSQL_SERVER_PASSWORD}"
+  debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_SERVER_PASSWORD}"
+
+  apt install -y mysql-server mono-runtime unzip git curl xinit xserver-xorg ttf-anonymous-pro unclutter fonts-fantasque-sans apache2 php5 libapache2-mod-php5 php5-gd php5-mysql php-fpdf libphp-phpmailer php5-cli php5-common php5-curl php5-json php5-ldap php5-readline
 
   echo -n ${flavor} > /usr/local/etc/happyshop_flavor
   echo "Selected flavor is $(cat /usr/local/etc/happyshop_flavor)"
@@ -39,6 +45,12 @@ sudo /bin/sh -e - << EOF
     cp /etc/rc.local /usr/local/etc/happyshop_rc.local_backup
   fi
   sed -i '20i (cd /usr/local/share/happyshop-rpibootstrap && git pull && ./happyshop-start-pos-client.sh)&\n' /etc/rc.local
+
+  echo "###"
+  echo "### The MySQL server password is ${MYSQL_SERVER_PASSWORD}"
+  echo "###"
+  echo "### Please write down this password before closing or clearing this terminal session."
+  echo "###"
 
 EOF
 
