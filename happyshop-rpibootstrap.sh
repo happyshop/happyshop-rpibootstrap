11b16237f5861a8c7afe5025ac146a1e7965eb02
Alexander Schleifer <alexander.schleifer@googlemail.com>
2017-04-25 11:19:09 +0200

    Update happyshop-rpibootstrap.sh

--- happyshop-rpibootstrap.sh
+++ happyshop-rpibootstrap.sh
@@ -22,13 +22,15 @@ sudo /bin/sh -e - << EOF
   apt update
   apt upgrade -y
 
-  debconf-set-selections <<< "mysql-server mysql-server/root_password password ${MYSQL_SERVER_PASSWORD}"
-  debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_SERVER_PASSWORD}"
+  echo "mysql-server mysql-server/root_password password ${MYSQL_SERVER_PASSWORD}" | debconf-set-selections
+  echo "mysql-server mysql-server/root_password_again password ${MYSQL_SERVER_PASSWORD}" | debconf-set-selections
 
   apt install -y mysql-server mono-runtime unzip git curl xinit xserver-xorg ttf-anonymous-pro unclutter fonts-fantasque-sans apache2 php5 libapache2-mod-php5 php5-gd php5-mysql php-fpdf libphp-phpmailer php5-cli php5-common php5-curl php5-json php5-ldap php5-readline
 
   echo -n ${flavor} > /usr/local/etc/happyshop_flavor
   echo "Selected flavor is $(cat /usr/local/etc/happyshop_flavor)"
+  
+  echo -n "${MYSQL_SERVER_PASSWORD}" > /usr/local/etc/mysql_root_password
 
   cd /usr/local/share
     if ! [ -d happyshop-rpibootstrap ]
@@ -57,6 +59,6 @@ sudo /bin/sh -e - << EOF
 EOF
 
 sync
-sudo reboot
+# sudo reboot
 
 exit 0
